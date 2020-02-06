f2d3ca672f5a5a2c9deba852eab2286a5a3984e6
Moto Ishizawa <summerwind.jp@gmail.com>
2020-02-06 22:15:26 +0900

    Unset environment variables for runner config

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -17,4 +17,6 @@ fi
 
 cd /runner
 ./config.sh --unattended --replace --name "${RUNNER_NAME}" --url "https://github.com/${RUNNER_REPO}" --token "${RUNNER_TOKEN}"
+
+unset RUNNER_NAME RUNNER_REPO RUNNER_TOKEN
 exec ./run.sh --once
