ec0915ce7c55c2ad263b60ba2b1c0a4ff7bec25d
Sebastien Le Digabel <sledigabel@gmail.com>
2021-08-02 10:11:48 +0100

    Adding some unit testing for entrypoint.sh
    The unit tests are simulating a run for entrypoint. It creates some
    dummy config.sh and runsvc.sh and makes sure the logic behind
    entrypoint.sh is correct.
    
    Unfortunately the entrypoint.sh contains some sections that are not
    mockable so I had to put some logic in there too.
    
    Testing includes for now:
    - the normal scenario
    - the normal non-ephemeral scenario
    - the configuration failure scenario
    
    Also tested the entrypoint.sh on a real runner, still works as expected.

---
+++ test/entrypoint/should_work_normally/test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# UNITTEST: should work normally
+# Will simulate a normal execution scenario. expects:
+# - the configuration step to be run exactly once
+# - the entrypoint script to exit with no error
+# - the runsvc.sh script to run with the --once flag activated.
+
+source ../logging.sh
+
+entrypoint_log() {
+  while read I; do
+    printf "\tentrypoint.sh: $I\n"
+  done
+}
+
+log "Setting up the test"
+export UNITTEST=true
+export RUNNER_HOME=localhome
+export RUNNER_NAME="example_runner_name"
+export RUNNER_REPO="myorg/myrepo"
+export RUNNER_TOKEN="xxxxxxxxxxxxx"
+
+mkdir -p ${RUNNER_HOME}/bin
+# add up the config.sh and runsvc.sh
+ln -s ../config.sh ${RUNNER_HOME}/config.sh
+ln -s ../../runsvc.sh ${RUNNER_HOME}/bin/runsvc.sh
+
+cleanup() {
+  rm -rf ${RUNNER_HOME}
+  unset UNITTEST
+  unset RUNNERHOME
+  unset RUNNER_NAME
+  unset RUNNER_REPO
+  unset RUNNER_TOKEN
+}
+
+trap cleanup SIGINT SIGTERM SIGQUIT EXIT
+
+log "Running the entrypoint"
+log ""
+
+../../../runner/entrypoint.sh 2> >(entrypoint_log)
+
+if [ "$?" != "0" ]; then
+  error "=========================="
+  error "Test completed with errors"
+  exit 1
+fi
+
+log "Testing if the configuration step was run only once"
+count=`cat ${RUNNER_HOME}/counter || echo "not_found"`
+if [ ${count} != "1" ]; then
+  error "==============================================="
+  error "The configuration step was not run exactly once"
+  exit 1
+fi
+
+success "The configuration ran ${count} time(s)"
+
+log "Testing if runsvc ran"
+if [ ! -f "${RUNNER_HOME}/runsvc_ran" ]; then
+  error "=============================="
+  error "The runner service has not run"
+  exit 1
+fi
+
+success "The service ran"
+success ""
+success "==========================="
+success "Test completed successfully"
