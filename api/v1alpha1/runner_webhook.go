a9ac5a1cbf98853f8de15e9f5fbb889b6bef85c8
Nikola Jokic <97525037+nikola-jokic@users.noreply.github.com>
2022-06-29 13:32:00 +0200

    extracted validations to a single point (#1582)

--- api/v1alpha1/runner_webhook.go
+++ api/v1alpha1/runner_webhook.go
@@ -66,25 +66,7 @@ func (r *Runner) ValidateDelete() error {
 
 // Validate validates resource spec.
 func (r *Runner) Validate() error {
-	var (
-		errList field.ErrorList
-		err     error
-	)
-
-	err = r.Spec.ValidateRepository()
-	if err != nil {
-		errList = append(errList, field.Invalid(field.NewPath("spec", "repository"), r.Spec.Repository, err.Error()))
-	}
-
-	err = r.Spec.ValidateWorkVolumeClaimTemplate()
-	if err != nil {
-		errList = append(errList, field.Invalid(field.NewPath("spec", "workVolumeClaimTemplate"), r.Spec.WorkVolumeClaimTemplate, err.Error()))
-	}
-
-	err = r.Spec.ValidateIsServiceAccountNameSet()
-	if err != nil {
-		errList = append(errList, field.Invalid(field.NewPath("spec", "serviceAccountName"), r.Spec.ServiceAccountName, err.Error()))
-	}
+	errList := r.Spec.Validate(field.NewPath("spec"))
 
 	if len(errList) > 0 {
 		return apierrors.NewInvalid(r.GroupVersionKind().GroupKind(), r.Name, errList)
