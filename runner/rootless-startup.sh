3d4f807ca820068bba2131ebc7400befc44208cb
Claudio Vellage <claudio.vellage@pm.me>
2022-10-12 12:10:38 +0200

    Update naming convention and make POOL_SIZE configurable

--- runner/rootless-startup.sh
+++ runner/rootless-startup.sh
@@ -19,8 +19,8 @@ if [ -n "${DOCKER_REGISTRY_MIRROR}" ]; then
 jq ".\"registry-mirrors\"[0] = \"${DOCKER_REGISTRY_MIRROR}\"" /home/runner/.config/docker/daemon.json > /tmp/.daemon.json && mv /tmp/.daemon.json /home/runner/.config/docker/daemon.json
 fi
 
-if [ -n "${DOCKER_DEFAULT_NETWORK_CIDR_RANGE}" ]; then
-jq ".\"default-address-pools\" = {\"base\": \"${DOCKER_DEFAULT_NETWORK_CIDR_RANGE}\", \"size\": 24}" /etc/docker/daemon.json > /tmp/.daemon.json && /tmp/.daemon.json /etc/docker/daemon.json
+if [ -n "${DOCKER_DEFAULT_ADDRESS_POOL_BASE}" ] && [ -n "${DOCKER_DEFAULT_ADDRESS_POOL_SIZE}" ]; then
+jq ".\"default-address-pools\" = [{\"base\": \"${DOCKER_DEFAULT_ADDRESS_POOL_BASE}\", \"size\": ${DOCKER_DEFAULT_ADDRESS_POOL_SIZE}}]" /etc/docker/daemon.json > /tmp/.daemon.json && /tmp/.daemon.json /etc/docker/daemon.json
 fi
 SCRIPT
 
