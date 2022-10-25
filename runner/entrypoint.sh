9c030044be6d14622193d1f96d2a4f494276a137
Yusuke Kuoka <ykuoka@gmail.com>
2022-10-25 02:11:25 +0000

    Fix dind sidecar to not hang in terminating when pod deleted before dockerd start

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -14,4 +14,17 @@ log.notice "Runner init started with pid $RUNNER_INIT_PID"
 wait $RUNNER_INIT_PID
 log.notice "Runner init exited. Exiting this process with code 0 so that the container and the pod is GC'ed Kubernetes soon."
 
+if [ -f /runner/.runner ]; then
+# If the runner failed with the following error:
+#   √ Connected to GitHub
+#   Failed to create a session. The runner registration has been deleted from the server, please re-configure.
+#   Runner listener exit with terminated error, stop the service, no retry needed.
+#   Exiting runner...
+# It might have failed to delete the .runner file.
+# We use the existence of the .runner file as the indicator that the runner agent has not stopped yet.
+# Remove it by ourselves now, so that the dockerd sidecar prestop won't hang waiting for the .runner file to appear.
+  echo "Removing the .runner file"
+  rm /runner/.runner
+fi
+
 trap - TERM
