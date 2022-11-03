42fdd76999606e8f7c525d570f28a02dc82df3e1
Yusuke Kuoka <ykuoka@gmail.com>
2022-11-03 05:39:09 +0000

    e2e: Bump runner version to 2.299.1

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -41,7 +41,7 @@ var (
 
 	testResultCMNamePrefix = "test-result-"
 
-	RunnerVersion = "2.297.0"
+	RunnerVersion = "2.299.1"
 )
 
 // If you're willing to run this test via VS Code "run test" or "debug test",
