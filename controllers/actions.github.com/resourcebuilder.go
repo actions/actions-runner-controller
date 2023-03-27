155114b593677a0fd57d679f4167243d080aa494
Nikola Jokic <jokicnikola07@gmail.com>
2023-03-27 16:14:16 +0200

    removed duplicate func from rebase

--- controllers/actions.github.com/resourcebuilder.go
+++ controllers/actions.github.com/resourcebuilder.go
@@ -547,25 +547,6 @@ func rulesForListenerRole(resourceNames []string) []rbacv1.PolicyRule {
 	}
 }
 
-func applyGitHubURLLabels(url string, labels map[string]string) error {
-	githubConfig, err := actions.ParseGitHubConfigFromURL(url)
-	if err != nil {
-		return fmt.Errorf("failed to parse github config from url: %v", err)
-	}
-
-	if len(githubConfig.Enterprise) > 0 {
-		labels[LabelKeyGitHubEnterprise] = githubConfig.Enterprise
-	}
-	if len(githubConfig.Organization) > 0 {
-		labels[LabelKeyGitHubOrganization] = githubConfig.Organization
-	}
-	if len(githubConfig.Repository) > 0 {
-		labels[LabelKeyGitHubRepository] = githubConfig.Repository
-	}
-
-	return nil
-}
-
 func autoscalingRunnerSetFullName(autoscalingRunnerSet *v1alpha1.AutoscalingRunnerSet) string {
 	return autoscalingRunnerSet.Name + "-" + autoscalingRunnerSet.Labels["app.kubernetes.io/name"]
 }
