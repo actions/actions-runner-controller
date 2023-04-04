a3c31545e04c6b0a69b7259cc52c705d759f042f
TingluoHuang <TingluoHuang@github.com>
2023-04-04 14:59:08 -0400

    remove deprecated method.

--- testing/random.go
+++ testing/random.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
 const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+
+var (
+	random = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
 
 // Copied from https://stackoverflow.com/a/31832326 with thanks
 func RandStringBytesRmndr(n int) string {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+		b[i] = letterBytes[random.Int63()%int64(len(letterBytes))]
 	}
 	return string(b)
 }
