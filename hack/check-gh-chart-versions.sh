5f5d51e38c2fc0cb9542773c8daaffbf685808f4
Nikola Jokic <jokicnikola07@gmail.com>
2023-04-19 15:20:45 +0200

    formatted and added colors

--- hack/check-gh-chart-versions.sh
+++ hack/check-gh-chart-versions.sh
@@ -2,6 +2,7 @@
 #
 TEXT_RED='\033[0;31m'
 TEXT_RESET='\033[0m'
+TEXT_GREEN='\033[0;32m'
 
 chart_dir=$(pwd)/charts
 
@@ -13,11 +14,11 @@ scaleset_app_version=$(cat $chart_dir/gha-runner-scale-set/Chart.yaml | yq .appV
 
 if [[ $controller_version != $controller_app_version ]] || [[ $controller_version != $scaleset_version ]] || [[ $controller_version != $scaleset_app_version ]]; then
     echo -e "${TEXT_RED}Chart versions do not match${TEXT_RESET}"
-    echo "Controller version:     " $controller_version
-    echo "Controller app version: " $controller_app_version
-    echo "Scale set version:      " $scaleset_version
-    echo "Scale set app version:  " $scaleset_app_version
+    echo "Controller version:     $controller_version"
+    echo "Controller app version: $controller_app_version"
+    echo "Scale set version:      $scaleset_version"
+    echo "Scale set app version:  $scaleset_app_version"
     exit 1
 fi
 
-echo "Chart versions: $controller_version"
+echo -e "${TEXT_GREEN}Chart versions: $controller_version"
