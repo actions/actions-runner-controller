51ac1a0aa6e293bff16fe79faeb0869f8134de31
Yusuke Kuoka <ykuoka@gmail.com>
2023-05-28 08:00:29 +0000

    Add the optional marker back for EffectiveTime anyway

--- apis/actions.summerwind.net/v1alpha1/horizontalrunnerautoscaler_types.go
+++ apis/actions.summerwind.net/v1alpha1/horizontalrunnerautoscaler_types.go
@@ -114,9 +114,11 @@ type PushSpec struct {
 // to the scale target until ExpirationTime.
 type CapacityReservation struct {
 	Name           string      `json:"name,omitempty"`
-	EffectiveTime  metav1.Time `json:"effectiveTime,omitempty"`
 	ExpirationTime metav1.Time `json:"expirationTime,omitempty"`
 	Replicas       int         `json:"replicas,omitempty"`
+
+	// +optional
+	EffectiveTime metav1.Time `json:"effectiveTime,omitempty"`
 }
 
 type ScaleTargetRef struct {
