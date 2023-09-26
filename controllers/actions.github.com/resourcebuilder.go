fdf7b6c5255fc912c3abae54905ae692bb7efc78
Nikola Jokic <jokicnikola07@gmail.com>
2023-09-26 11:21:16 +0200

    Fix nil map when annotations are applied (#2916)
    Co-authored-by: Hidetake Iwata <int128@gmail.com>

--- controllers/actions.github.com/resourcebuilder.go
+++ controllers/actions.github.com/resourcebuilder.go
@@ -317,7 +317,9 @@ func (b *resourceBuilder) newScaleSetListenerPod(autoscalingListener *v1alpha1.A
 }
 
 func mergeListenerPodWithTemplate(pod *corev1.Pod, tmpl *corev1.PodTemplateSpec) {
-	// apply metadata
+	if pod.Annotations == nil {
+		pod.Annotations = make(map[string]string)
+	}
 	for k, v := range tmpl.Annotations {
 		if _, ok := pod.Annotations[k]; !ok {
 			pod.Annotations[k] = v
