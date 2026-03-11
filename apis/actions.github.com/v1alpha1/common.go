821fae4e1e6ddff5760515857ce55c3be2081342
Nikola Jokic <jokicnikola07@gmail.com>
2026-03-11 17:38:49 +0100

    Add omitempty

--- apis/actions.github.com/v1alpha1/common.go
+++ apis/actions.github.com/v1alpha1/common.go
@@ -3,7 +3,7 @@ package v1alpha1
 // ResourceMeta carries metadata common to all internal resources
 type ResourceMeta struct {
 	// +optional
-	Labels map[string]string `json:"labels"`
+	Labels map[string]string `json:"labels,omitempty"`
 	// +optional
-	Annotations map[string]string `json:"annotations"`
+	Annotations map[string]string `json:"annotations,omitempty"`
 }
