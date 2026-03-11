b8b51380dc4ef4456561d9ec62379695db8a5657
Nikola Jokic <jokicnikola07@gmail.com>
2026-03-11 17:07:42 +0100

    make things optional

--- apis/actions.github.com/v1alpha1/common.go
+++ apis/actions.github.com/v1alpha1/common.go
@@ -2,6 +2,8 @@ package v1alpha1
 
 // ResourceMeta carries metadata common to all internal resources
 type ResourceMeta struct {
-	Labels      map[string]string `json:"labels"`
+	// +optional
+	Labels map[string]string `json:"labels"`
+	// +optional
 	Annotations map[string]string `json:"annotations"`
 }
