276717a04b4a98df1e48f3947c8282ef5ec6d08e
Nikola Jokic <jokicnikola07@gmail.com>
2026-03-16 10:09:36 +0100

    Manually bump dependencies since it needs fixes related to the controller runtime API (#4406)

--- controllers/actions.summerwind.net/runner_controller.go
+++ controllers/actions.summerwind.net/runner_controller.go
@@ -26,6 +26,7 @@ import (
 	"time"
 
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/client-go/tools/events"
 
 	"github.com/actions/actions-runner-controller/build"
 	"github.com/actions/actions-runner-controller/hash"
@@ -33,7 +34,6 @@ import (
 
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -66,7 +66,7 @@ const (
 type RunnerReconciler struct {
 	client.Client
 	Log                         logr.Logger
-	Recorder                    record.EventRecorder
+	Recorder                    events.EventRecorder
 	Scheme                      *runtime.Scheme
 	GitHubClient                *MultiGitHubClient
 	Name                        string
@@ -408,7 +408,14 @@ func (r *RunnerReconciler) processRunnerCreation(ctx context.Context, runner v1a
 		return ctrl.Result{}, err
 	}
 
-	r.Recorder.Event(&runner, corev1.EventTypeNormal, "PodCreated", fmt.Sprintf("Created pod '%s'", newPod.Name))
+	r.Recorder.Eventf(
+		&runner,
+		nil,
+		corev1.EventTypeNormal,
+		"PodCreated",
+		"",
+		fmt.Sprintf("Created pod '%s'", newPod.Name),
+	)
 	log.Info("Created runner pod", "repository", runner.Spec.Repository)
 
 	return ctrl.Result{}, nil
@@ -423,14 +430,28 @@ func (r *RunnerReconciler) createObject(ctx context.Context, obj client.Object, 
 	if err := r.Create(ctx, obj); err != nil {
 		if kerrors.IsAlreadyExists(err) {
 			log.Info(fmt.Sprintf("Failed to create %s %s as it already exists. Reusing existing %s", kind, meta.Name, kind))
-			r.Recorder.Event(runner, corev1.EventTypeNormal, fmt.Sprintf("%sReused", kind), fmt.Sprintf("Reused %s '%s'", kind, meta.Name))
+			r.Recorder.Eventf(
+				runner,
+				nil,
+				corev1.EventTypeNormal,
+				fmt.Sprintf("%sReused", kind),
+				"",
+				fmt.Sprintf("Reused %s '%s'", kind, meta.Name),
+			)
 			return nil
 		}
 
 		log.Error(err, fmt.Sprintf("Retrying as failed to create %s %s resource", kind, meta.Name))
 		return &ctrl.Result{Requeue: true}
 	}
-	r.Recorder.Event(runner, corev1.EventTypeNormal, fmt.Sprintf("%sCreated", kind), fmt.Sprintf("Created %s '%s'", kind, meta.Name))
+	r.Recorder.Eventf(
+		runner,
+		nil,
+		corev1.EventTypeNormal,
+		fmt.Sprintf("%sCreated", kind),
+		"",
+		fmt.Sprintf("Created %s '%s'", kind, meta.Name),
+	)
 	log.Info(fmt.Sprintf("Created %s", kind), "name", meta.Name)
 	return nil
 }
@@ -453,7 +474,14 @@ func (r *RunnerReconciler) updateRegistrationToken(ctx context.Context, runner v
 		//    POST https://api.github.com/enterprises/YOUR_ENTERPRISE/actions/runners/registration-token: 403 Resource not accessible by integration []
 		// In such case retrying in seconds might not make much sense.
 
-		r.Recorder.Event(&runner, corev1.EventTypeWarning, "FailedUpdateRegistrationToken", "Updating registration token failed")
+		r.Recorder.Eventf(
+			&runner,
+			nil,
+			corev1.EventTypeWarning,
+			"FailedUpdateRegistrationToken",
+			"",
+			"Updating registration token failed",
+		)
 		log.Error(err, "Failed to get new registration token")
 		return false, err
 	}
@@ -472,7 +500,14 @@ func (r *RunnerReconciler) updateRegistrationToken(ctx context.Context, runner v
 		return false, err
 	}
 
-	r.Recorder.Event(&runner, corev1.EventTypeNormal, "RegistrationTokenUpdated", "Successfully update registration token")
+	r.Recorder.Eventf(
+		&runner,
+		nil,
+		corev1.EventTypeNormal,
+		"RegistrationTokenUpdated",
+		"",
+		"Successfully update registration token",
+	)
 	log.Info("Updated registration token", "repository", runner.Spec.Repository)
 
 	return true, nil
@@ -818,7 +853,6 @@ func newRunnerPodWithContainerMode(containerMode string, template corev1.Pod, ru
 
 	if runnerSpec.DockerVarRunVolumeSizeLimit == nil {
 		runnerSpec.DockerVarRunVolumeSizeLimit = resource.NewScaledQuantity(1, resource.Mega)
-
 	}
 
 	// Be aware some of the environment variables are used
@@ -1290,7 +1324,7 @@ func (r *RunnerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		name = r.Name
 	}
 
-	r.Recorder = mgr.GetEventRecorderFor(name)
+	r.Recorder = mgr.GetEventRecorder(name)
 
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Runner{}).
